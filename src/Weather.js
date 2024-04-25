e31fd5ee6bcb7c7abd2ddf304895f16faab392ed
Ana Cañete <ana.canete13@gmail.com>
2024-04-25 11:26:25 -0400

    A search engine was added to the Weather.js row

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,27 @@ import axios from "axios";
 
 export default function Weather(props) {
 
+    function handleSubmit(event) {
+        
+    }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <div className="row m-5">
+        <div className="col-6 ">
+          <input
+            type="search"
+            placeholder="Enter a city.."
+            className="form-control"
+          />
+        </div>
+        <div className="col-1">
+          <input type="submit" value="Search" className="btn btn-primary" />
+        </div>
+      </div>
+    </form>
+  );
+
   function showTemperature(response) {
     console.log(response.data.main.temp);
   }
@@ -11,5 +32,5 @@ export default function Weather(props) {
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tokio&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemperature);
-  return(<h1>hola</h1>)
+  return <div className="Weather">{form}</div>;
 }
