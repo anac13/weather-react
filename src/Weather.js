f6320c63d21ba606d632f902456fe098bf56dc93
Ana Cañete <ana.canete13@gmail.com>
2024-04-26 10:47:49 -0400

    added a search engine and style to the page

--- src/Weather.js
+++ src/Weather.js
@@ -1,36 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import "./App.css";
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState(null);
 
-export default function Weather(props) {
-
-    function handleSubmit(event) {
-        
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (city.length > 0) {
+      let apiKey = "3a94f3778290bfeee61278505dbbe51d";
+      let units = "metric";
+      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+      axios.get(apiUrl).then(displayWeather);
+    } else {
+      alert("Enter city...");
     }
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
 
   let form = (
     <form onSubmit={handleSubmit}>
       <div className="row m-5">
-        <div className="col-6 ">
+        <div className="col-9 ">
           <input
             type="search"
             placeholder="Enter a city.."
             className="form-control"
+            onChange={updateCity}
           />
         </div>
         <div className="col-1">
-          <input type="submit" value="Search" className="btn btn-primary" />
+          <button type="Submit" className="btn btn-primary">Search</button>
         </div>
       </div>
     </form>
   );
 
-  function showTemperature(response) {
-    console.log(response.data.main.temp);
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      name: response.data.name,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
   }
 
-  let apiKey = "3a94f3778290bfeee61278505dbbe51d";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tokio&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showTemperature);
-  return <div className="Weather">{form}</div>;
+  if (loaded) {
+    return (
+      <div className="Weather">
+        {form}
+        <h1>{weather.name}</h1>
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}</li>
+          <li>Description: {weather.description}</li>
+          <li>Wind: {weather.wind}</li>
+          <li>Humidity: {weather.humidity}</li>
+          <li>
+            <img src={weather.icon} alt={weather.description}/>
+          </li>
+        </ul>
+      </div>
+    );
+     
+     
+  } else {
+    return <div className="Weather">
+      {form}
+    </div>;
+  }
 }
