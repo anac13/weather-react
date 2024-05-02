520ee0e49a2054cfbd228ba7acfe5fea54453e3c
Ana Cañete <ana.canete13@gmail.com>
2024-05-02 13:26:56 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+import React from "react";
 import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Weather />
-      Coded by Ana Cañete and is
-      <a
-        href="https://github.com/anac13/weather-react"
-        target="_blank"
-        rel="noreferrer"
-      >
-        {" "}
-        open-sourced on GitHub
-      </a>
+      <div className="container">
+        <Weather />
+        Coded by Ana Cañete and is
+        <a
+          href="https://github.com/anac13/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </div>
     </div>
   );
 }
 
+export default App;
