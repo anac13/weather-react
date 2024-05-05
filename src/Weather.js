95c1e35352308c2114b181d4ec6f96d9f1ac8321
Ana Cañete <ana.canete13@gmail.com>
2024-05-04 20:13:34 -0400

    Improved code structure

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
+
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
 
@@ -38,30 +39,8 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={weatherData.date} />
-          </li>
-          <li className="text-capitalize">{weatherData.description} </li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6">
-            <img src={weatherData.iconUrl} alt={weatherData.description} />
-
-            <span className="temperature">
-              {Math.round(weatherData.temperature)}
-            </span>
-            <span className="unit">C</span>
-          </div>
-
-          <div className="col-6">
-            <ul>
-              <li>Wind: {weatherData.wind}%</li>
-              <li>Humidity: {weatherData.humidity} km/h</li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo  data= {weatherData}/>
+      
       </div>
     );
   } else {
